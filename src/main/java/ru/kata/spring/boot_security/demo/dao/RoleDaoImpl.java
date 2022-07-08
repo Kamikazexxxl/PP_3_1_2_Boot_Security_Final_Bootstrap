5fc8d1908390091edd91f216607842c2fce93544
Kamikazexxxl <kamikazexxxl@gmail.com>
2022-07-08 22:45:47 +0300

    1

---
+++ src/main/java/ru/kata/spring/boot_security/demo/dao/RoleDaoImpl.java
@@ -0,0 +1,39 @@
+package ru.kata.spring.boot_security.demo.dao;
+
+import ru.kata.spring.boot_security.demo.models.Role;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class RoleDaoImpl implements RoleDao {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Override
+    public void addRole(Role role) {
+        entityManager.persist(role);
+    }
+
+    @Override
+    public Role findRole(String role) {
+        return entityManager.createQuery("select role from Role role where role.name=:role", Role.class)
+                .setParameter("role", role).getSingleResult();
+    }
+
+    @Override
+    public void deleteRole(Role role) {
+        entityManager.remove(role);
+    }
+
+    @Override
+    public Set<Role> getAllRoles() {
+        List<Role> list = entityManager
+                .createQuery("select role from Role role", Role.class)
+                .getResultList();
+        return new HashSet<>(list);
+    }
+}
